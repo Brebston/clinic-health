5b5c01e7a803d82b0ac21589e0fd339ffb7ca8bf
Yurii Palahitskyi <brebston7@gmail.com>
2025-12-13 12:46:51 +0100

    Added the ability to edit profile and authorisation by email.

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.contrib.auth.views import LogoutView
+from django.urls import path, reverse_lazy
+from accounts.views import CustomLoginView, RegisterView, ProfileDetailView
+
+
+urlpatterns = [
+    path("login/", CustomLoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path(
+        "logout/",
+        LogoutView.as_view(next_page=reverse_lazy("accounts:login")),
+        name="logout",
+    ),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("profile/<int:pk>", ProfileDetailView.as_view(), name="profile"),
+] + debug_toolbar_urls()
+
+app_name = "accounts"
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+from django.views.generic import CreateView
+from .forms import LoginForm, RegisterForm
+from .models import Patient
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    template_name = "accounts/register.html"
+    form_class = RegisterForm
+    success_url = reverse_lazy("accounts:profile")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # авто-логін після реєстрації
+        login(self.request, self.object)
+        return response
+
+class ProfileDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = "accounts/profile.html"
+    model = Patient
+
+
+class CustomLoginView(LoginView):
+    template_name = "accounts/login.html"
+    authentication_form = LoginForm
+
+    def get_success_url(self):
+        return reverse("accounts:profile", kwargs={"pk": self.request.user.pk})
